af50cd05f1cdb00d0f1830b9e395c01f6f617cbf
AlexDev39 <alexhavilla@gmail.com>
2020-08-30 07:13:35 -0300

    First class: Possition

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import bordgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Position ps = new Position(3, 5);
+		System.out.println(ps);
+
+	}
+
+}
